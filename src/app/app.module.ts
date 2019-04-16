2009c3e9f6bd5a65fc750a530835fe4f844f6591
nastya <nast.ovc@yandex.ru>
2019-04-16 14:33:31 +0400

    app structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { UsersModule } from './users/users.module';
+import { CoursesModule } from './courses/courses.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    UsersModule,
+    CoursesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
