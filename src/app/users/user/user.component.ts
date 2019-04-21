24ca5a8a47e426ee126b8484c3151d6b2f44bca4
nastya <nast.ovc@yandex.ru>
2019-04-21 09:54:12 +0400

    user login display added

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user.model';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, User {
+  id: number;
+  firstName: string;
+  lastName: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.id=1548;
+    this.firstName = 'Anastasiia';
+    this.lastName = 'Parsaeva';
   }
 
 }
