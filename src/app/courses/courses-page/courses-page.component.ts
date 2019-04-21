26e13ea6144f010422c794648aeca7089134176b
nastya <nast.ovc@yandex.ru>
2019-04-21 09:57:50 +0400

    fake values assignment in ngOnInit added

--- src/app/courses/courses-page/courses-page.component.ts
+++ src/app/courses/courses-page/courses-page.component.ts
@@ -8,60 +8,61 @@ import { CourseItem } from '../course-item.model';
 })
 export class CoursesPageComponent implements OnInit {
 
-  public courseItems: CourseItem[] = [
-    {
-      id: 1005,
-      title: 'Video Course 1',
-      duration: 88,
-      creationDate: new Date(),
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-      nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-      mollit anim id est laborum.`
-    },
-    {
-      id: 1006,
-      title: 'Video Course 2',
-      duration: 109,
-      creationDate: new Date('06.09.2018'),
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-      nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-      mollit anim id est laborum.`
-    },
-    {
-      id: 1007,
-      title: 'Video Course 3',
-      duration: 99,
-      creationDate: new Date('09.18.2018'),
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-      nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-      mollit anim id est laborum.`
-    },
-    {
-      id: 1008,
-      title: 'Video Course 4',
-      duration: 55,
-      creationDate: new Date('07.15.2018'),
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-      nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-      mollit anim id est laborum.`
-    }
-  ];
+  public courseItems: CourseItem[];
 
   constructor() { }
 
   ngOnInit() {
+    this.courseItems = [
+      {
+        id: 1005,
+        title: 'Video Course 1',
+        duration: 88,
+        creationDate: new Date(),
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+        mollit anim id est laborum.`
+      },
+      {
+        id: 1006,
+        title: 'Video Course 2',
+        duration: 109,
+        creationDate: new Date('06.09.2018'),
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+        mollit anim id est laborum.`
+      },
+      {
+        id: 1007,
+        title: 'Video Course 3',
+        duration: 99,
+        creationDate: new Date('09.18.2018'),
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+        mollit anim id est laborum.`
+      },
+      {
+        id: 1008,
+        title: 'Video Course 4',
+        duration: 55,
+        creationDate: new Date('07.15.2018'),
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+        mollit anim id est laborum.`
+      }
+    ];
   }
 
   deleteCourseItem(courseItem) {
