44c7f9356d6cf9ae2d1a4237d2d7b1b746e92e59
Anastasiia Parsaeva <anastasiia_parsaeva@epam.com>
2019-05-07 17:29:05 +0400

    authorization service added

--- src/app/users/auth.service.ts
+++ src/app/users/auth.service.ts
@@ -6,4 +6,14 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  login() { }
+
+  logout() { }
+
+  isAuthentificated(): boolean { 
+    return false;
+  }
+
+  getUserInfo() { }
 }
