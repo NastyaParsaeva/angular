ec0112ed6d70f9f1b6d8d5bd69534e0ec8b232e9
nastya <nast.ovc@yandex.ru>
2019-05-13 21:54:57 +0400

    auth guard added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,36 @@ import { CoursesPageComponent } from './courses/courses-page/courses-page.compon
 import { LoginPageComponent } from './login/login-page/login-page.component';
 import { AddCoursePageComponent } from './add-course/add-course-page/add-course-page.component';
 import { PageNotFoundPageComponent } from './page-not-found/page-not-found-page/page-not-found-page.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const ROUTES: Route[] = [
-    { path: 'courses', component: CoursesPageComponent },
-    { path: 'login', component: LoginPageComponent},
-    { path: 'courses/new', component: AddCoursePageComponent},
-    { path: '', redirectTo: 'courses', pathMatch: 'full' },
-    { path: 'courses/:id', component: AddCoursePageComponent, data: { 'security_key': 'key_here'} },
-    { path: '**', component: PageNotFoundPageComponent }
-
+    { 
+        path: 'courses', 
+        component: CoursesPageComponent ,
+        canActivate: [ AuthGuard ],
+    },
+    { 
+        path: 'login', 
+        component: LoginPageComponent
+    },
+    { 
+        path: 'courses/new', 
+        component: AddCoursePageComponent,
+        canActivate: [ AuthGuard ],
+    },
+    { 
+        path: '', 
+        redirectTo: 'courses', 
+        pathMatch: 'full' 
+    },
+    { 
+        path: 'courses/:id', 
+        component: AddCoursePageComponent, 
+        data: { 'security_key': 'key_here'} ,
+        canActivate: [ AuthGuard ],
+    },
+    { 
+        path: '**', 
+        component: PageNotFoundPageComponent 
+    }
 ]
