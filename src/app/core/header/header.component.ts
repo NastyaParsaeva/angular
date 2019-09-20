93716c54ae5085026982a0e7333c8cfeca63615d
Anastasiia Parsaeva <anastasiia_parsaeva@epam.com>
2019-09-21 00:50:47 +0400

    user subscription moved to header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth.service';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  public userId: number;
+  public userFirstName: string;
+  public userLastName: string;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
-  ngOnInit() {
+  public ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if(event instanceof NavigationStart) {
+        this.getUserInfo();
+      }
+    });
+  }
+
+  public getUserInfo() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      this.authService.getUserInfo(token).subscribe(response => {
+        this.userFirstName = response.name.first;
+        this.userLastName = response.name.last;
+        this.userId = response.id;
+      });
+    }
+  }
+
+  public isAuthentificated(): boolean {
+    return this.authService.isAuthentificated();
+  }
+
+  public logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('login');
   }
 
 }
--- src/app/core/user/user.component.ts
+++ src/app/core/user/user.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, OnChanges, DoCheck, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { User } from '../user.model';
-import { AuthService } from '../../shared/auth.service';
-import { Router, NavigationStart } from '@angular/router';
 import { ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
@@ -10,38 +8,19 @@ import { ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit, User {
-  id: number;
-  firstName: string;
-  lastName: string;
+  @Input() public id: number;
+  @Input() public firstName: string;
+  @Input() public lastName: string;
+  @Input() public isAuthentificated: boolean;
+  @Output() onLogOutClick: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor(private authService: AuthService, private router: Router) { 
+  constructor() { 
   }
 
   ngOnInit() {
-    this.router.events.subscribe((event) => {
-      if(event instanceof NavigationStart) {
-        this.getUserInfo();
-      }
-    });
   }
 
-  logout() {
-    this.authService.logout();
-    this.router.navigateByUrl('login');
-  }
-
-  isAuthentificated(): boolean {
-    return this.authService.isAuthentificated();
-  }
-
-  getUserInfo() {
-    const token = localStorage.getItem('token');
-
-    if (token) {
-      this.authService.getUserInfo(token).subscribe(response => {
-        this.firstName = response.name.first;
-        this.lastName = response.name.last;
-      });
-    }
+  handleLogoutClick() {
+    this.onLogOutClick.emit();
   }
 }
